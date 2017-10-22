dd7826150d0616f92f161bae01e0ccd1a8656e6f
tdog191 <twarther17@gmail.com>
2017-10-21 21:34:02 -0500

    I completed a basic draft of the function for registration.  To make further progress on it, we will need to agree on the structure of the tables in our database and the format of a valid email address.

--- databass-flask/flask_api.py
+++ databass-flask/flask_api.py
@@ -1,6 +1,11 @@
+# Import all necessary Flask libraries and extensions
 from flask import Flask
-from flask.ext.mysql import MySQL
-from flask.ext.api import status
+from flask.ext.mysql import MySQL # Connects Flask server to MySQL database
+from flask.ext.api import status # Handles error codes returned by Flask server
+
+from string import ascii_letters
+from string import digits
+import re
 
 app = Flask(__name__)
 
@@ -13,21 +18,59 @@ app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 mysql.init_app(app)
 
 
-'''
-@app.route("/")
-def hello():
-    cursor = mysql.connect().cursor()
-    cursor.execute('''SELECT * FROM world_cities WHERE City="paris"''')
-    results = cursor.fetchall()
-    return "Hello World! This is Flask." +
-           "<br/>Here are all the cities in our database named Paris:<br/>" +
-           "".join([s + "<br/>" for s in str(results).split("), (")])
-'''
-
 # User Registration
-@app.route("/api/user/register")
+@app.route("/api/user/register", Method="POST")
 def register():
-    return 0
+    # Read in registration input parameters
+    username = request.form.get('username') # String (a-z, A-Z, 0-9, -, _)
+    password = request.form.get('password') # String (6 <= characters <= 256)
+    email_address = request.form.get('email_address') # String (valid email)
+    display_name = request.form.get('display_name') # String (1 <= characters <= 265)
+
+    # Check if all the registration input parameters are valid
+
+    # Check if username is valid
+    if not all((c in ascii_letters + digits + '-' + '_') for c in username):
+        error_code = "user_register_invalid_username"
+
+        content = {"success": false, "error_code": error_code}
+        return content, status.HTTP_400_BAD_REQUEST
+
+    # Check if password is valid
+    if not (len(password) >= 6 and len(password) <= 256):
+        error_code = "user_register_invalid_password"
+
+        content = {"success": false, "error_code": error_code}
+        return content, status.HTTP_400_BAD_REQUEST
+
+    # Check if email_address is valid
+    emailRegex = re.compile(r"([a-zA-Z0-9]+)@([a-zA-Z0-9]+)\.([a-zA-Z0-9]+)")
+
+    if not emailRegex.match(email_address):
+        error_code = "user_register_invalid_email"
+
+        content = {"success": false, "error_code": error_code}
+        return content, status.HTTP_400_BAD_REQUEST
+
+    # Check if display_name is valid
+    if not (len(display_name) >= 1 and len(display_name) <= 265):
+        error_code = "user_register_invalid_display_name"
+
+        content = {"success": false, "error_code": error_code}
+        return content, status.HTTP_400_BAD_REQUEST
+
+    # If this line of the register() function is reached,
+    # all the registration input parameters are valid.
+
+    # Connect to MySQL database and insert
+    # registration information into Users table
+    cursor = mysql.connect().cursor()
+    cursor.execute("INSERT INTO Users values('" +
+                    username + "', '" + password + "', '" +
+                    email_address + "', '" + display_name + "'")
+
+    content = {"success": true}
+    return content, status.HTTP_200_OK
 
 
 # User Login
@@ -46,3 +89,15 @@ def profile():
 @app.route("/api/user/checkin")
 def checkin():
     return 0
+
+
+'''
+@app.route("/")
+def hello():
+    cursor = mysql.connect().cursor()
+    cursor.execute('''SELECT * FROM world_cities WHERE City="paris"''')
+    results = cursor.fetchall()
+    return "Hello World! This is Flask." +
+           "<br/>Here are all the cities in our database named Paris:<br/>" +
+           "".join([s + "<br/>" for s in str(results).split("), (")])
+'''
