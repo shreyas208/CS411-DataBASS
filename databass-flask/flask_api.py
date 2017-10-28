2d5a87bf5728d467fa799fb4b348076b1b9b4ca7
tdog191 <twarther17@gmail.com>
2017-10-27 19:35:39 -0500

    This is the latest version of the file from our group meeting.

--- databass-flask/flask_api.py
+++ databass-flask/flask_api.py
@@ -2,24 +2,27 @@
 from flask import Flask
 from flask.ext.mysql import MySQL # Connects Flask server to MySQL database
 from flask.ext.api import status # Handles error codes returned by Flask server
+from flask.ext.bcrypt import Bcrypt
 
 from string import ascii_letters
 from string import digits
 import re
 
 app = Flask(__name__)
+bcrypt = Bcrypt(app)
 
 mysql = MySQL()
 
+app.debug = True
 app.config['MYSQL_DATABASE_USER'] = 'flaskuser'
 app.config['MYSQL_DATABASE_PASSWORD'] = 'tCU8PvBYEPP4qkun'
-app.config['MYSQL_DATABASE_DB'] = 'srpatil2_cities_test'
+app.config['MYSQL_DATABASE_DB'] = 'cs_411_project'
 app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 mysql.init_app(app)
 
 
 # User Registration
-@app.route("/api/user/register", Method="POST")
+@app.route("/api/user/register", methods=["POST"])
 def register():
     # Read in registration input parameters
     username = request.form.get('username') # String (a-z, A-Z, 0-9, -, _)
@@ -36,7 +39,7 @@ def register():
     if not all((c in ascii_letters + digits + '-' + '_') for c in username):
         error_code = "user_register_invalid_username"
 
-        content = {"success": false, "error_code": error_code}
+        content = {"success": False, "error_code": error_code}
         return content, status.HTTP_400_BAD_REQUEST
 
     # Check if the username is new
@@ -46,15 +49,17 @@ def register():
     if result:
         error_code = "user_register_username_in_use"
 
-        content = {"success": false, "error_code": error_code}
+        content = {"success": False, "error_code": error_code}
         return content, status.HTTP_400_BAD_REQUEST
 
     # Check if password is valid
     if not (len(password) >= 6 and len(password) <= 256):
         error_code = "user_register_invalid_password"
 
-        content = {"success": false, "error_code": error_code}
+        content = {"success": False, "error_code": error_code}
         return content, status.HTTP_400_BAD_REQUEST
+
+    password_hash = bcrypt.generate_password_hash(password)
 
     # Check if email_address is valid
     emailRegex = re.compile(r"([a-zA-Z0-9]+)@([a-zA-Z0-9]+)\.([a-zA-Z0-9][a-zA-Z0-9][a-zA-Z0-9])")
@@ -62,14 +67,14 @@ def register():
     if not emailRegex.match(email_address):
         error_code = "user_register_invalid_email"
 
-        content = {"success": false, "error_code": error_code}
+        content = {"success": False, "error_code": error_code}
         return content, status.HTTP_400_BAD_REQUEST
 
     # Check if display_name is valid
     if not (len(display_name) >= 1 and len(display_name) <= 265):
         error_code = "user_register_invalid_display_name"
 
-        content = {"success": false, "error_code": error_code}
+        content = {"success": False, "error_code": error_code}
         return content, status.HTTP_400_BAD_REQUEST
 
     # If this line of the register() function is reached,
@@ -80,12 +85,12 @@ def register():
                     username + "', '" + password + "', '" +
                     email_address + "', '" + display_name + "'")
 
-    content = {"success": true}
+    content = {"success": True}
     return content, status.HTTP_200_OK
 
 
 # User Login
-@app.route("/api/user/login", Method="POST")
+@app.route("/api/user/login", methods=["POST"])
 def login():
     # Read in login input parameters
     username = request.form.get('username') # String (a-z, A-Z, 0-9, -, _)
@@ -100,14 +105,14 @@ def login():
     if not all((c in ascii_letters + digits + '-' + '_') for c in username):
         error_code = "user_login_invalid_username"
 
-        content = {"success": false, "error_code": error_code}
+        content = {"success": False, "error_code": error_code}
         return content, status.HTTP_400_BAD_REQUEST
 
     # Check if password is valid
     if not (len(password) >= 6 and len(password) <= 256):
         error_code = "user_login_invalid_password"
 
-        content = {"success": false, "error_code": error_code}
+        content = {"success": False, "error_code": error_code}
         return content, status.HTTP_400_BAD_REQUEST
 
     # If this line of the login() function is reached,
@@ -120,35 +125,36 @@ def login():
     if not result:
         error_code = "user_login_bad_credentials"
 
-        content = {"success": false, "error_code": error_code}
+        content = {"success": False, "error_code": error_code}
         return content, status.HTTP_400_BAD_REQUEST
     else:
         email = result[0]
         display_name = result[1]
         access_token = result[2]
-        content = {"success": true, "email_address": email, "display_name": display_name, "access_token": access_token}
+        content = {"success": True, "email_address": email, "display_name": display_name, "access_token": access_token}
         return content, status.HTTP_200_OK
 
 
 # User Profile
-@app.route("/api/user/profile", Method="POST")
+@app.route("/api/user/profile", methods=["POST"])
 def profile():
     return 0
 
 
 # City Checkin
-@app.route("/api/user/checkin", Method="POST")
+@app.route("/api/user/checkin", methods=["POST"])
 def checkin():
     return 0
 
-
-'''
 @app.route("/")
-def hello():
-    cursor = mysql.connect().cursor()
-    cursor.execute('''SELECT * FROM world_cities WHERE City="paris"''')
-    results = cursor.fetchall()
-    return "Hello World! This is Flask." +
-           "<br/>Here are all the cities in our database named Paris:<br/>" +
-           "".join([s + "<br/>" for s in str(results).split("), (")])
-'''
+def root():
+    return "You have reached our Flask server."
+
+#@app.route("/")
+#def hello():
+#    cursor = mysql.connect().cursor()
+#    cursor.execute('''SELECT * FROM world_cities WHERE City="paris"''')
+#    results = cursor.fetchall()
+#    return "Hello World! This is Flask." +
+#           "<br/>Here are all the cities in our database named Paris:<br/>" +
+#           "".join([s + "<br/>" for s in str(results).split("), (")])
