f7918affdb79dbba63822b38b8ed38e3939bf5df
Eric Auster <auster.eric@gmail.com>
2017-12-06 19:36:35 -0600

    Updated loginResponse.java

--- databass-android/app/src/main/java/com/shreyas208/databass/api/model/LoginResponse.java
+++ databass-android/app/src/main/java/com/shreyas208/databass/api/model/LoginResponse.java
@@ -9,13 +9,14 @@ import com.google.gson.annotations.SerializedName;
 public class LoginResponse {
 
     private final boolean success;
-    @SerializedName("error_code") private String errorCode;
+    @SerializedName("error_code") private final String errorCode;
     @SerializedName("email_address") private final String emailAddress;
     @SerializedName("display_name") private final String displayName;
     @SerializedName("access_token") private final String accessToken;
 
-    public LoginResponse(boolean success, String emailAddress, String displayName, String accessToken) {
+    public LoginResponse(boolean success, String errorCode, String emailAddress, String displayName, String accessToken) {
         this.success = success;
+        this.errorCode = errorCode;
         this.emailAddress = emailAddress;
         this.displayName = displayName;
         this.accessToken = accessToken;
