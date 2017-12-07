7a655fd1dc78011e0334553b08b58c1c86d8e1f5
Gautham Bommannan <gautham.bommannan@gmail.com>
2017-12-06 20:14:44 -0600

    still debugging

--- databass-flask/databass_api.py
+++ databass-flask/databass_api.py
@@ -1068,7 +1068,7 @@ def email_verify():
 # -------------------------------------------------------------------------------------------------------------------- #
 #email verification request
 def send_email_verif(email_address, email_token):
-    headers = {'Authorization', 'Zoho-authtoken a61e6765b5f28645c9621a7057f29973'}
+    headers = {'Authorization': 'Zoho-authtoken a61e6765b5f28645c9621a7057f29973'}
 
     payload = {
     "fromAddress" : "travelations@shreyas208.com",
@@ -1079,7 +1079,7 @@ def send_email_verif(email_address, email_token):
     "content": "Welcome to Travelation!<br />Please verify your account <a href=\"http://fa17-cs411-18.cs.illinois.edu/verify?email_token=" + email_token + "\">here</a>."
     }
 
-    r = requests.post('https://mail.zoho.com/api/accounts/5601770000000008001/messages', params=payload, headers=headers)
+    r = requests.post('https://mail.zoho.com/api/accounts/5601770000000008001/messages', json=payload, headers=headers)
 
 # -------------------------------------------------------------------------------------------------------------------- #
 
